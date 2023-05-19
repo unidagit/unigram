279f0d68ede7e95e11b528d9d69ef9e77db91bfe
unidagit <rhcjrdbsgml@gmail.com>
2023-05-19 14:36:42 +0900

    Feat : button size 속성 추가

--- src/components/ui/colorButton/ColorButton.tsx
+++ src/components/ui/colorButton/ColorButton.tsx
@@ -3,13 +3,21 @@ import React from "react";
 type Props = {
   text: string;
   onClick: () => void; // 반환하는 값이 없음.
+  size?: "small" | "big"; //1.size가 small이거나 big. 유니온 타입
 };
 
-export default function ColorButton({ text, onClick }: Props) {
+export default function ColorButton({ text, onClick, size = "small" }: Props) {
+  //2.size를 명시하지 않으면 기본 설정 small
   return (
-    <div className="rounded-md bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300 p-[0.15rem]">
+    <div
+      className={`rounded-md bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300
+    ${size === "big" ? "p-[0.3rem]" : "p-[0.15rem]"}
+    `}
+    >
       <button
-        className="bg-white rounded-sm text-base p-[0.3rem] hover:opacity-90 transition-opacity"
+        className={`bg-white rounded-sm  p-[0.3rem] hover:opacity-90 transition-opacity
+        ${size === "big" ? "p-4 text-2xl" : "p-[0.3rem] text-base"}
+        `}
         onClick={onClick}
       >
         {text}
