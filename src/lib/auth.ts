e57e38fe875a398225a91f2be228441ca627e46e
unidagit <rhcjrdbsgml@gmail.com>
2023-05-19 20:13:57 +0900

    Feat : addUser 구현

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,3 +1,4 @@
+import { addUser } from "@/service/user";
 import type { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
@@ -9,6 +10,19 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
   callbacks: {
+    async signIn({ user: { id, name, image, email } }) {
+      if (!email) {
+        return false;
+      }
+      addUser({
+        id,
+        name: name || "",
+        image,
+        email,
+        username: email.split("@")[0],
+      });
+      return true;
+    },
     async session({ session }) {
       console.log(session);
       const user = session?.user;
